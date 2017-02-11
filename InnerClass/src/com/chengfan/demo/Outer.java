d02821f82e5f578b2e5cd0e596c2a685f175c926
CleverFan <377109151@qq.com>
2017-02-11 14:46:35 +0800

    daydayup

---
+++ InnerClass/src/com/chengfan/demo/Outer.java
@@ -0,0 +1,45 @@
+package com.chengfan.demo;
+
+/**
+ * @author chengfan
+ * @create 2017-02-11 下午2:08
+ */
+public class Outer {
+    private String a = "a";
+    public int i = 1;
+
+    class Inner{
+        private String b = "b";
+        public String c = "c";
+
+        public int getInt(){
+            return i; // 内部类可以访问外部类变量
+        }
+
+        private String getString(){
+            return a + b + c; // 内部类可以访问外部类的private变量
+        }
+    }
+
+    public String getParam(){
+        Inner inner = new Inner();
+        inner.b = "bb"; // 外部类可以访问内部类的private变量
+        inner.c = "cc";
+        return inner.getInt() + inner.getString();
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+        System.out.println(outer.getParam()); // 输出：1abbcc
+
+        Outer.Inner oi = outer.new Inner();
+        oi.c = "ccc";
+        //oi.b = "bbb";  编译失败
+        System.out.println(oi.getInt()); // 输出：1
+        //System.out.println(oi.getString()); 编译失败
+    }
+}
+
+
